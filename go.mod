394a055b2a73f8698a57eab0fa91d264419e4260
Maxc040 <Max.cornelissen@student.fontys.nl>
2023-05-17 15:08:23 +0200

    Code versie 1.0

---
+++ go.mod
@@ -0,0 +1,11 @@
+module sharepoint
+
+go 1.19
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/oauth2 v0.8.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,1 +1,96 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+
+	"golang.org/x/oauth2" //
+)
+
+const (
+	clientID        = "9fff275d-98e9-435c-ac69-426203a13c75"                         //Client-ID krijg ik wanneer ik de App-regrstratie maak
+	clientSecret    = "eO98Q~Ks0cASPwqgFQiqp-p1gs9fqOGbNaek-apv"                     //Deze krijg ik wanneer ik een secret-key aanmaak in de App-regristratie
+	redirectURL     = "http://localhost:8080/callback"                               //Deze moet ik aanmaken wanneer ik de App-regrstratie aanmaakte. Deze is default
+	authorizeURL    = "https://twsr4.sharepoint.com/_layouts/15/oauthauthorize.aspx" //Hierdoor krijg ik de Authorisatie-code
+	tokenURL        = "https://twsr4.sharepoint.com/_layouts/15/OAuthToken.aspx"     //Hierdoor krijg ik de Authorisatie-token, hiervoor heb ik wel de code nodig
+	siteCreationURL = "https://twsr4-admin.sharepoint.com/_api/SPSiteManager/create" //Via deze link maakt hij de SharePoint site aan, deze is gekoppeld aan mijn eigen tennant
+)
+
+// in dit stukje zal ik met de informatie hierboven de oauth2 instellen
+func main() {
+	config := &oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  authorizeURL,
+			TokenURL: tokenURL,
+		},
+		RedirectURL: redirectURL,
+		Scopes:      []string{"AllSites.Manage"}, //hier geef ik alle rechten die overeen moeten komen met diegene die ik geef in de app-regristratie
+	}
+
+	// Start de autorisatiecode-stroom
+	authURL := config.AuthCodeURL("state", oauth2.AccessTypeOffline)
+	fmt.Printf("Open deze URL in je browser om in te loggen: \n%s\n", authURL)
+
+	// Start de webserver om de callback te ontvangen
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		code := r.URL.Query().Get("code")
+		_, err := config.Exchange(context.Background(), code)
+		if err != nil {
+			log.Fatal("Fout bij het uitwisselen van autorisatiecode: ", err)
+		}
+
+		// Hier moet ik de gekregen code geven, hierna zal ik een Token moeten krijgen
+		fmt.Println("hier komt de authorisatiecode die ik krijg wanneer ik de code run:", code)
+
+		// Creëer een nieuwe SharePoint-site
+		err = createSite(code)
+		if err != nil {
+			log.Fatal("Fout bij het maken van de SharePoint-site: ", err)
+		}
+
+		fmt.Println("SharePoint-site succesvol aangemaakt!")
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+// Functie om een nieuwe SharePoint-site aan te maken
+func createSite(authorizationCode string) error {
+	client := &http.Client{}
+
+	siteURL := "https://twsr4.sharepoint.com/sites/your-site" //mijn tennant zijn SharePoint-link
+
+	// Aanvraag maken voor het maken van een nieuwe SharePoint-site
+	req, err := http.NewRequest("POST", siteCreationURL, nil)
+	if err != nil {
+		return err //voorbeeld errorhandling
+	}
+
+	req.Header.Add("Authorization", "Bearer "+authorizationCode)
+	req.Header.Add("Accept", "application/json;odata=verbose")
+	req.Header.Add("Content-Type", "application/json;odata=verbose")
+
+	// Sitegegevens configureren
+	body := strings.NewReader(`{"url": "` + siteURL + `", "title": "Mijn SharePoint-site", "description": "Dit is mijn nieuwe SharePoint-site"}`)
+	req.Body = ioutil.NopCloser(body)
+	req.ContentLength = int64(body.Len())
+
+	// Aanvraag versturen
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Fout bij het maken van de SharePoint-site: %s", resp.Status)
+	}
+
+	return nil
+}
